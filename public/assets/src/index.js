a8f4643e8e5b8d76dc2a2074fc24d0f284e953f0
walterrodriguesjr <walterrjr.86@gmail.com>
2022-09-09 10:42:23 -0300

    aula-3

--- public/assets/src/index.js
+++ public/assets/src/index.js
@@ -1,4 +1,4 @@
-var name = "Walter";
-var year = 1986;
-var age = 36;
-alert(`Meu nome é ${name}, tenho ${age} de idade e nasci no ano de ${year}.`);
+function person(){
+    var documentCpf = "34040011848";
+}
+person(documentCpf);
